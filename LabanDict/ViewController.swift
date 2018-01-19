47f5e6ba2572d0c9dc2bb0b9028ddf99e0c7f3a9
Binh Nguyen <blcsntb@gmail.com>
2018-01-19 13:09:22 +0700

    Initial Commit

---
+++ LabanDict/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LabanDict
+//
+//  Created by Binh Nguyen on 1/19/18.
+//  Copyright © 2018 Binh Nguyen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
